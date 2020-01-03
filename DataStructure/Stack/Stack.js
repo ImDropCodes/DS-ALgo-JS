920a1f368b17a185362add689eb7eb2d8c64fc4e
DropCodes <sagardb50@gmail.com>
2020-01-03 11:07:16 +0530

    added Readmee.md and Stack commants

--- DataStructure/Stack/Stack.js
+++ DataStructure/Stack/Stack.js
@@ -1,12 +1,35 @@
+/*
+ * @author Sagar D B (DropCodes)
+ * Github: github.com/imdropcodes
+ * Date: 03/01/2020
+ * 
+ * Implementing Stack using Array
+ * 
+ * Stack : 
+ * It is a DataStructure. Which user LIFO (Last In First Out) ordering,
+ * most recently added item into stack will removed first,
+ * At starting stack top will be pointing to -1
+ * 
+ * there are 4 function of stack:
+ * 
+ * pop() : Remove top top item of the stack.
+ * push(item): Add the item into top of the stack.
+ * peek(): Return the top item of the stack.
+ * isEmpty(): it return true only if stack is empty ie top pointing to -1.
+ * 
+ */
+
 class Stack{
     
     constructor(){
         this.array = [];
     }
 
+	// Pushing an Element into stack
     push(element){
         this.array.push(element);
     }
+    // Pop an Element from stack
     pop(){
         if(this.array.length == 0){
             return "Stack UnderFlow";
@@ -14,13 +37,16 @@ class Stack{
             return this.array.pop();
         }
     }
+    // return the top element of stack
     peek(){
         return this.array[this.array.length - 1];
     }
+    // this check if stck is empty
     isEmpty(){
         return this.array.length == 0;
     }
 
+    // For printing stack element
     printStack(){
         let res = " ";
         for(let i = 0; i < this.array.length ; i++ ){
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # DS-ALgo-JS
- DataStructure and Algorithm
+ DataStructure and Algorithm using JavaScript
+
+ |DataStructure | link|
+ |---|---|
+ |Stack using Array|[Click here](https://github.com/ImDropCodes/DS-ALgo-JS/blob/master/DataStructure/Stack/Stack.js)
+ 
+
